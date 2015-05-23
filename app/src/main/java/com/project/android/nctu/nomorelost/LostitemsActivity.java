743b16644fa908f7f7bf3abe1b196e136e36046e
chichunchen <chi.chun_chen844@icloud.com>
2015-05-23 17:48:44 +0800

    Add lostitems activity.

--- app/src/main/java/com/project/android/nctu/nomorelost/LostitemsActivity.java
+++ app/src/main/java/com/project/android/nctu/nomorelost/LostitemsActivity.java
@@ -1,26 +1,24 @@
 package com.project.android.nctu.nomorelost;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 
-public class MainActivity extends AppCompatActivity {
+public class LostitemsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_lostitems);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_lostitems, menu);
         return true;
     }
 
@@ -37,10 +35,5 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    public void lostItems(View view) {
-        Intent intent = new Intent(this, LostitemsActivity.class);
-        startActivity(intent);
     }
 }
