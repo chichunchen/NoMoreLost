1d7eb5d244356b61921c631c889ade5d15e5b066
chichunchen <chi.chun_chen844@icloud.com>
2015-05-25 17:03:06 +0800

    Add upload lostitem activity.

--- app/src/main/java/com/project/android/nctu/nomorelost/uploadLostItem.java
+++ app/src/main/java/com/project/android/nctu/nomorelost/uploadLostItem.java
@@ -1,26 +1,33 @@
 package com.project.android.nctu.nomorelost;
 
-import android.content.Intent;
+import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 
 
-public class MainActivity extends AppCompatActivity {
+public class uploadLostItem extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_upload_lost_item);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new PlaceholderFragment())
+                    .commit();
+        }
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_upload_lost_item, menu);
         return true;
     }
 
@@ -39,13 +46,19 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    public void lostItems(View view) {
-        Intent intent = new Intent(this, LostitemsListActivity.class);
-        startActivity(intent);
-    }
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
 
-    public void uploadLostItem(View view) {
-        Intent intent = new Intent(this, uploadLostItem.class);
-        startActivity(intent);
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_upload_lost_item, container, false);
+            return rootView;
+        }
     }
 }
