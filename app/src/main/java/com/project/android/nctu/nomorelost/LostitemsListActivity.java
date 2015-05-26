0c8fa529635828fa97c13122d288971685f1be7f
chichunchen <chi.chun_chen844@icloud.com>
2015-05-26 16:21:01 +0800

    Fix bug when 'appcompat does not support current features', implement listener in lostitem list activity.

--- app/src/main/java/com/project/android/nctu/nomorelost/LostitemsListActivity.java
+++ app/src/main/java/com/project/android/nctu/nomorelost/LostitemsListActivity.java
@@ -1,20 +1,28 @@
 package com.project.android.nctu.nomorelost;
 
 
-import android.app.Activity;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.FragmentManager;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.SearchView;
 import android.widget.SimpleAdapter;
 import android.widget.TextView;
+import android.support.v7.widget.Toolbar;
 
 import com.loopj.android.http.JsonHttpResponseHandler;
 import com.project.android.nctu.nomorelost.utils.ApiRequestClient;
+import com.project.android.nctu.nomorelost.utils.ToolsHelper;
+import com.yalantis.contextmenu.lib.ContextMenuDialogFragment;
+import com.yalantis.contextmenu.lib.MenuObject;
+import com.yalantis.contextmenu.lib.interfaces.OnMenuItemClickListener;
+import com.yalantis.contextmenu.lib.interfaces.OnMenuItemLongClickListener;
 
 import org.apache.http.Header;
 import org.json.JSONArray;
@@ -23,15 +31,22 @@ import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 
-public class LostitemsListActivity extends Activity {
+public class LostitemsListActivity extends AppCompatActivity implements SearchView.OnQueryTextListener, OnMenuItemClickListener, OnMenuItemLongClickListener {
 
     private final String TAG = "LostitemsListActivity";
     private ListView mListView;
     public JSONArray lostitems;
     private ArrayList<HashMap<String, Object>> list = new ArrayList<HashMap<String, Object>>();
 
+    private boolean isInitialized = false;
+    private FragmentManager mFragmentManager;
+    private DialogFragment mMenuDialogFragment;
+    private TextView mTitle;
+    private SearchView mSearch;
+    private ImageView mMenu;
     private SimpleAdapter adapter;
     private TextView textViewCategory, textViewContact, textViewDescription;
 
@@ -40,9 +55,74 @@ public class LostitemsListActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.lostitems_list_activity);
 
+        mFragmentManager = getSupportFragmentManager();
+        mMenuDialogFragment = ContextMenuDialogFragment.newInstance((int) getResources()
+                                        .getDimension(R.dimen.tool_bar_height), getMenuObjects());
+        initToolbar();
+
         findView();
         getLostitemsList();
     }
+
+    private List<MenuObject> getMenuObjects() {
+        List<MenuObject> menuObjects = new ArrayList<>();
+        menuObjects.add(new MenuObject("生活及運動用品"));
+        menuObjects.add(new MenuObject("衣物、手錶與配件"));
+        menuObjects.add(new MenuObject("現金、證件、票卡"));
+        menuObjects.add(new MenuObject("圖書文具"));
+        menuObjects.add(new MenuObject("3C"));
+        menuObjects.add(new MenuObject("鑰匙、其他"));
+
+        MenuObject refresh = new MenuObject();
+        refresh.setResource(R.drawable.ic_refresh_blue_36dp);
+        menuObjects.add(refresh);
+
+        return menuObjects;
+    }
+
+    private void initToolbar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        mTitle =  (TextView)findViewById(R.id.title);
+
+        mMenu = (ImageView)findViewById(R.id.menu);
+        mMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mMenuDialogFragment.show(mFragmentManager, "DropDownMenuFragment");
+            }
+        });
+
+        mSearch = (SearchView)findViewById(R.id.search);
+        mSearch.addOnLayoutChangeListener(searchExpandHandler);
+        mSearch.setOnQueryTextListener(this);
+        mSearch.setOnCloseListener(new SearchView.OnCloseListener() {
+            @Override
+            public boolean onClose() {
+                mTitle.setVisibility(View.VISIBLE);
+                mMenu.setVisibility(View.VISIBLE);
+                return false;
+            }
+        });
+
+        int searchImgId = getResources().getIdentifier("android:id/search_button", null, null);
+        ImageView v = (ImageView) mSearch.findViewById(searchImgId);
+        v.setImageResource(R.drawable.ic_search_white_36dp);
+    }
+
+    private final View.OnLayoutChangeListener searchExpandHandler = new View.OnLayoutChangeListener() {
+        @Override
+        public void onLayoutChange(View v, int left, int top, int right, int bottom, int oldLeft, int oldTop, int oldRight,
+                                   int oldBottom) {
+            SearchView searchView = (SearchView)v;
+            if (searchView.isIconfiedByDefault() && !searchView.isIconified())            {
+                // search got expanded from icon to search box, hide tabs to make space
+                mTitle.setVisibility(View.GONE);
+                mMenu.setVisibility(View.GONE);
+            }
+        }
+    };
 
     private void getLostitemsList() {
         String url = "http://52.68.136.81:3000/api/lostitems";
@@ -114,6 +194,36 @@ public class LostitemsListActivity extends Activity {
         }
     };
 
+    @Override
+    public void onWindowFocusChanged(boolean focus) {
+        super.onWindowFocusChanged(focus);
+        // ContentView has loaded
+        if(!isInitialized){
+            isInitialized = true;
+            initUI();
+            update(null);
+        }
+    }
+
+    private void initUI() {
+
+    }
+
+    public void update(View view) {
+        if(!ToolsHelper.isNetworkAvailable(this)) {
+            ToolsHelper.showNetworkErrorMessage(this);
+            finish();
+        } else {
+//            new UpdateTask(this).execute(getType());
+//            findViews();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        //mAdapter.notifyDataSetChanged();
+    }
+
     private void findView() {
         mListView = (ListView)findViewById(R.id.lostitem_list);
         textViewCategory = (TextView) findViewById(R.id.lostitem_category);
@@ -126,24 +236,35 @@ public class LostitemsListActivity extends Activity {
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_lostitems, menu);
-        return true;
+    public void onMenuItemClick(View view, int position) {
+        switch (position) {
+            case 0:
+                break;
+            case 1:
+//                this.info(null);
+                break;
+            case 2:
+//                this.update(null);
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+        }
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    public void onMenuItemLongClick(View view, int i) {
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+    }
 
-        return super.onOptionsItemSelected(item);
+    @Override
+    public boolean onQueryTextSubmit(String s) {
+        return false;
+    }
+
+    @Override
+    public boolean onQueryTextChange(String s) {
+        return false;
     }
 }
