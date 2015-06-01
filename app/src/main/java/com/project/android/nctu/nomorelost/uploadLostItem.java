6663580a4c4ca9c76a7c0e0336cb3b8236f64cff
David Lin <geniusdavid869@gmail.com>
2015-06-01 21:14:42 +0800

    Change Activity_upload_lost_item to have a scroll UI.

--- app/src/main/java/com/project/android/nctu/nomorelost/uploadLostItem.java
+++ app/src/main/java/com/project/android/nctu/nomorelost/uploadLostItem.java
@@ -2,6 +2,9 @@ package com.project.android.nctu.nomorelost;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.ContextMenu.ContextMenuInfo;
+import android.widget.Spinner;
+import android.widget.ArrayAdapter;
 
 
 public class UploadLostItem extends Activity {
@@ -10,5 +13,21 @@ public class UploadLostItem extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_upload_lost_item);
+
+        Spinner SpinnerS = (Spinner)findViewById(R.id.spinner01);
+
+        //設定功能表項目陣列，使用createFromResource()
+
+        ArrayAdapter adapter = ArrayAdapter.createFromResource(this, R.array.lostitem_category,
+
+                android.R.layout.simple_spinner_item);
+
+        //設定選單
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        //設定adapter
+
+        SpinnerS.setAdapter(adapter);
     }
 }
